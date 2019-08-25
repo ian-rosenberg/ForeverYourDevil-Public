c98630f3987484db12a02a2b6e26f40f4c5993f4
oi86039 <39169568+oi86039@users.noreply.github.com>
2019-08-24 23:04:49 -0400

    Cel/Toon Shaders,Day/Night Cycle
    Found Cel Shaders from Roystan (https://roystan.net/articles/toon-shader.html)
    Light Source rotates from east to west (Sped up for demonstration)

---
+++ Assets/DayNightRotator.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DayNightRotator : MonoBehaviour
+{
+    [Tooltip("Speed at which light rotates at")]
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.up, -speed);
+    }
+}
