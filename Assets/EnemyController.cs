4748c8b3d69c0bf5cbf26981fb99bb7431db11d5
Omar <oai4@njit.edu>
2020-01-15 01:36:55 -0500

    Enemy Behavior, Line of Sight, Encouter Transition[D[D[D[D[D[D[D[D[D[D[D[D[D[Dn[C[C[C[C[C[C[C[C[C[C[C[C[C ()[Dhalfway)

---
+++ Assets/EnemyController.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    gameManager gameManager;
+
+    public float enemySpeed;
+    public GameObject encounter;
+    Animator anim;
+    public FieldOfView fov;
+
+    public enum enemyState { IDLE, SPOTTED, RUNNING, COMBAT }
+    public enemyState state;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameManager = gameManager.Instance;
+        state = enemyState.IDLE;
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //If see player, change to spotted and running
+        if (state == enemyState.IDLE)
+        {
+            if (fov.canSee)
+            {
+                state = enemyState.SPOTTED;
+                anim.SetTrigger("Spotted");
+            }
+        }
+        if (state == enemyState.RUNNING)
+        {
+            //Move towards seen target
+            transform.position = Vector3.Lerp(transform.position,
+                fov.visibleTargets[fov.visibleTargets.Count - 1].position
+                , enemySpeed);
+
+            //If lost sight, go back to idle
+            if (!fov.canSee) {
+                state = enemyState.IDLE;
+                anim.SetTrigger("Unspotted");
+            }
+        }
+
+        //Have sprite always look toward the camera to appear 2d
+        if (encounter.activeSelf)
+        {
+            encounter.transform.LookAt(Camera.main.transform);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer == 10) //Player
+        {
+            Debug.Log("Call Combat Trigger");
+            gameManager.TriggerCombat(gameObject);
+        }
+
+    }
+
+    //0 = Idle, 1 = Spotted, 2 = running
+    public void ChangeState(int desiredState)
+    {
+        if (desiredState == 0) state = enemyState.IDLE;
+        if (desiredState == 1) state = enemyState.SPOTTED;
+        if (desiredState == 2) state = enemyState.RUNNING;
+        if (desiredState == 3) state = enemyState.COMBAT;
+    }
+}
--- Assets/_Scripts/gameManager.cs
+++ Assets/_Scripts/gameManager.cs
@@ -1,11 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class gameManager : MonoBehaviour
 {
+    bool canPause = true; //Allow pausing?
 
     public GameObject pauseMenu;
+    public Animator battleAnim; //Canvas for battle transition
+    public Image screenCapRegion; //Place to display image on
+    public GameObject player; //Reference to player character
+
+    public GameObject normalWorld; //Represents overworld
+    public GameObject battleWorld; //Represents battlefield
 
     private static gameManager instance;
     public static gameManager Instance
@@ -38,17 +46,20 @@ public class gameManager : MonoBehaviour
         if (Input.GetButtonDown("Pause"))
         {
             Debug.Log("Pressed Enter/Escape");
-            if (isPaused) UnPauseGame();
-            else PauseGame();
+            if (isPaused) {
+                pauseMenu.SetActive(false);
+                UnPauseGame(); }
+            else {
+                pauseMenu.SetActive(true);
+                PauseGame(); }
         }
 
     }
 
     public void PauseGame()
     {
-        if (!isPaused)
+        if (!isPaused && canPause)
         {
-            pauseMenu.SetActive(true);
             prevState = gameState;
             gameState = STATE.PAUSED;
             isPaused = true;
@@ -59,9 +70,8 @@ public class gameManager : MonoBehaviour
     }
     public void UnPauseGame()
     {
-        if (isPaused)
+        if (isPaused && canPause)
         {
-            pauseMenu.SetActive(false);
             gameState = prevState;
             prevState = STATE.PAUSED;
             isPaused = false;
@@ -71,4 +81,36 @@ public class gameManager : MonoBehaviour
         }
     }
 
+    public void SetCanPause(bool pause)
+    {
+        canPause = pause;
+    }
+
+    public void TriggerCombat(GameObject enemy)
+    {
+        // StartCoroutine(ScreenCap());
+        Debug.Log("Triggering Combat");
+        PauseGame();
+        SetCanPause(false);
+        battleAnim.SetTrigger("Battle");
+    }
+
+    IEnumerator ScreenCap()
+    {
+        {
+            yield return new WaitForEndOfFrame();
+            var texture = ScreenCapture.CaptureScreenshotAsTexture();
+            Sprite sprite = Sprite.Create(texture,
+                new Rect(0, 0, Screen.currentResolution.width,
+                Screen.currentResolution.height),
+                new Vector2(0, 0)
+                );
+
+            // do something with texture
+            screenCapRegion.sprite = sprite;
+
+            // cleanup
+            Object.Destroy(texture);
+        }
+    }
 }
