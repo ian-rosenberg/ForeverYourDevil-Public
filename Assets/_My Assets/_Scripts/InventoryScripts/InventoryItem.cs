4e0afd39cc9df222f2c3c75c333bd06a86e0c454
ian.rosenberg <isr5@njit.edu>
2020-05-21 16:01:27 -0400

    Inventory setting up

---
+++ Assets/_My Assets/_Scripts/InventoryScripts/InventoryItem.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+/*
+ * InventoryItem - Instance of an item in the inventory.
+ * 
+ * Author - Ian Rosenberg
+ */
+
+public class InventoryItem : MonoBehaviour
+{
+    //items
+    [Header("Inventory Item")]
+    private int id; //id referencing an item from 
+    private ItemType itemType;// Categorization for switch cases
+    private GameObject owner;//who owns this item?
+    private GameObject ownerSlot;//the inventory slot that holds this item
+
+    public string itemName;//another way to reference item, by name
+
+    public enum ItemType//What kind of item are we?
+    {
+        Concoction,
+        Equipment,
+        Ability
+    }
+
+    
+    public void UseItem()
+    {
+        //lets apply any effects on the player
+        //owner.GetComponent<PlayerController>().ApplyEffect();
+
+        switch(itemType)
+        {
+            case ItemType.Concoction:
+                //affect stats
+                break;
+
+            case ItemType.Equipment:
+                //Wear equipment
+                break;
+
+            case ItemType.Ability:
+                //Toggle Equip ability
+                break;
+
+            default://shouldn't get here
+                break;
+
+        }
+    }
+}
---
+++ Assets/_My Assets/_Scripts/InventoryScripts/PersonalInventory.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Linq;
+using System.Threading;
+/*
+ * InventoryItem - Instance of an item in the inventory.
+ * 
+ * Author - Ian Rosenberg
+ */
+
+public class PersonalInventory : MonoBehaviour
+{
+	[Header("Personal Inventory")]
+	private Dictionary<object, GameObject> inventoryItems;
+	
+	public GameObject elementOwnerPrefab;//The grid space in which an item can reside
+	public uint numSlots;//how many slots do we start with?
+	
+	private void Start()
+	{
+		inventoryItems = new Dictionary<object, GameObject>();
+		
+		Populate();
+	}
+
+	private void Update()
+	{
+
+	}
+
+	private void Populate()
+	{
+		ExpandInventory((int)numSlots);
+	}
+
+	private void AddSlot()
+	{
+		GameObject slotClone = Instantiate(elementOwnerPrefab, transform);
+		uint currentIndex = (uint)inventoryItems.Count();
+		//string n = slotClone.GetComponent<InventoryItem>().itemName;
+
+		inventoryItems.Add(currentIndex, slotClone);
+		//inventoryItems.Add(n, slotClone);
+	}
+
+	public void ExpandInventory(int slots)
+	{
+		for (int i = 0; i < slots; i++)
+		{
+			AddSlot();
+		}
+	}
+}
