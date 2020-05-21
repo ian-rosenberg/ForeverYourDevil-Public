0c77cb9e42b3719db2e7e6a973296ef5cbe81d2e
Omar <oai4@njit.edu>
2020-05-21 15:10:25 -0400

    Interactable Doors

---
+++ Assets/InteractDoor.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.Remoting;
+using UnityEngine;
+
+public class InteractDoor : Door
+{
+    public GameObject talkIndicator;
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.layer == 10 && gm.gameState == gameManager.STATE.TRAVELING) //10 = player
+        {
+            talkIndicator.SetActive(true);
+
+            if (Input.GetButtonDown("Interact"))
+            {
+                Activate();
+                talkIndicator.SetActive(false);
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.layer == 10)
+        {
+            talkIndicator.SetActive(false);
+        }
+    }
+
+}
+
